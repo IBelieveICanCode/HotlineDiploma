429abef9f167c6af541d348b68cbc3531777e869
OhHiMark <Zervanseryi@gmail.com>
2019-05-24 22:12:59 +0300

    Cube movement+, obstacle removed

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,25 +4,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    //public Transform Player;
-    //private Vector3
-    //    _offset,
-    //    _cameraMove;
 
-    //void Start()
-    //{
-    //    _offset = transform.position - Player.position;
-    //}
-    //void Update()
-    //{
-    //    CameraInGamePosition();
-    //}
-
-    //private void CameraInGamePosition()
-    //{
-    //    _cameraMove = Player.position + _offset;
-    //    transform.position = _cameraMove;
-    //}
 
     private void Update()
     {
